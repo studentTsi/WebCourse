9c0ce77842fcf67a637b9a74a67893dd37c2f324
studentTsi <sasha.vnk@inbox.lv>
2019-12-12 14:21:50 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const sections = [
+  ...document.getElementById("container").getElementsByTagName("section"),
+];
+const navSections = [
+  ...document.getElementById("nav-links").getElementsByTagName("li"),
+];
+
+const getCurrentViewArea = scroll => {
+  let currentSection;
+  sections.forEach(section => {
+    if (
+      scroll + window.innerHeight / 2 >= section.offsetTop &&
+      scroll + window.innerHeight / 2 < section.offsetTop + section.offsetHeight
+    ) {
+      currentSection = section;
+    }
+  });
+  return currentSection;
+};
+
+const addCurrentViewClass = element => {
+  navSections.forEach(item => {
+    if (element.id === item.dataset.value) {
+      item.classList.add("current-view");
+    } else {
+      item.classList.remove("current-view");
+    }
+  });
+};
+
+// Current view area calculation on every scroll
+window.addEventListener("scroll", e => {
+  const currentViewArea = getCurrentViewArea(window.pageYOffset);
+  addCurrentViewClass(currentViewArea);
+});
+
+//Initial class calculation
+window.addEventListener("load", e => {
+  const currentViewArea = getCurrentViewArea(window.pageYOffset);
+  addCurrentViewClass(currentViewArea);
+});
+
+//Smooth scrolling
+navSections.forEach(navElelement => {
+  navElelement.addEventListener("click", e => {
+    e.preventDefault();
+    const targetSection = sections.find(
+      section => section.id === navElelement.dataset.value,
+    );
+    const distanceTo = targetSection.getBoundingClientRect().top;
+    window.scrollBy({ top: distanceTo, left: 0, behavior: "smooth" });
+  });
+});
+
+const callToAction = () => {
+  const distanceTo = document.getElementById("about").getBoundingClientRect()
+    .top;
+  window.scrollBy({ top: distanceTo, left: 0, behavior: "smooth" });
+};
